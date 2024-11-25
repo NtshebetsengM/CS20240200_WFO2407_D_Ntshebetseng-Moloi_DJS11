b5dc165ff42f74b9098c08c2d1c143363bf8fbfa
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-11-25 16:33:05 +0200

    add sidebar, styling and toggle functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,39 @@
 
 import styles from "../styles/Header.module.css"
-import { Links, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import { useState } from "react"
 
 export function Header() {
+
+    const [isOpen, setIsOpen ] = useState(false)
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
       <header className={styles.header}>
           <div><h2 className={styles.logo}> ListenUp </h2> </div>
           <div>
-            <svg className={styles.hamburgerIcon} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none">
-                <path d="M4 18L20 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/>
-                <path d="M4 12L20 12" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/>
-                <path d="M4 6L20 6" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/>
-            </svg>
+            <button className={styles.hamburgerIcon} onClick={toggleSidebar}>
+                <svg  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none">
+                    <path d="M4 18L20 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M4 12L20 12" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M4 6L20 6" stroke="#ffffff" stroke-width="2" stroke-linecap="round"/>
+                </svg>
+            </button>
            </div>
            <nav className= {styles.navLinks} >
                 <NavLink to="/" > Home</NavLink>
                 <NavLink to="favourites"> Favourites</NavLink>
                 <NavLink to="filter">Genres</NavLink>
                 <NavLink to="reset">reset</NavLink>
-
            </nav>
-           
+           <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="favourites"> Favourites</NavLink>
+                <NavLink to="filter">Genres</NavLink>
+                <NavLink to="reset">reset</NavLink>
+           </div>
       </header>
   
   )
