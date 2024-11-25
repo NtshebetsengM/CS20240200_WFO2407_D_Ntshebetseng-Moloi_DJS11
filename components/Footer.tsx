2e1ae592a6ffac3c86d8e2889d2225fa313864f3
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-11-25 16:49:38 +0200

    add footer and styling with links and copywright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,17 @@
+import "../styles/Footer.module.css"
+import { Link } from "react-router-dom"
+
 export function Footer() {
     return (
-      <>
-           <h1>this the footer</h1>   
-      </>
+      <footer>
+        <p>Discover the best podcasts, all in one place.</p>
+                <Link to="/" >Home</Link> | 
+                <Link to="favourites"> Favourites</Link> |
+                <Link to="filter"> Genres </Link> |
+                <Link to="reset"> Reset</Link>
+           <p> 2024 &copy; ListenUp. All rights reserved.</p>   
+           
+      </footer>
   
   )
   }
