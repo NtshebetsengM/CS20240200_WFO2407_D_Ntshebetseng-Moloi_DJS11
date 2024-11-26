3aa98555f69c3f54fcb3e6a29088cacb04c31e8f
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-11-26 10:52:51 +0200

    update footer and header styling. add home page template

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,39 @@
 import styles from "../styles/Home.module.css"
 
-
+//podcasts need to be sorted alphabetically
+//preview img
 export function Home() {
   return (
     <>
-        <h2>home page</h2>
+    <input type="text" />
+    <button>search</button>
+        <h2>discover</h2>
+        <ul>
+          <li>
+              <div>
+                <img src="podcast.png" alt="" />
+                <h3>title</h3>
+                <p>seasons | genre</p>
+                <p>last update date</p>
+              </div>
+            </li>
+          <li>
+            <div>
+                <img src="podcast.png" alt="" />
+                <h3>title</h3>
+                <p>seasons | genre</p>
+                <p>last update date</p>
+              </div>
+              </li>
+          <li>
+            <div>
+                <img src="podcast.png" alt="" />
+                <h3>title</h3>
+                <p>seasons | genre</p>
+                <p>last update date</p>
+              </div>
+          </li>
+        </ul>
 
     </>
 
